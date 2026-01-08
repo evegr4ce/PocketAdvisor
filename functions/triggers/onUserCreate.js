a992697d65334502cbad903acd490985db840245
Helin Taskesen <helintaskesen@gmail.com>
2026-01-08 11:45:21 -0500

    attempted user creations function trigger

--- functions/triggers/onUserCreate.js
+++ functions/triggers/onUserCreate.js
@@ -1,1 +1,21 @@
 // new user signup initialization
+const admin = require("firebase-admin");
+const db = admin.firestore();
+
+function onUserCreate(user) {
+  const userRef = db.collection("users").doc(user.uid);
+
+  return userRef.set(
+    {
+      email: user.email,
+      createdAt: admin.firestore.FieldValue.serverTimestamp(),
+      settings: {
+        theme: "light",
+        notifications: true,
+      },
+    },
+    { merge: true }
+  );
+}
+
+module.exports = onUserCreate;
